6b2d88d0e2c71fe49fc704677db2aa4d590746a7
M Hamza <ha243695@gmail.com>
2025-11-23 12:17:40 +0500

    Deadlock Prevention (Bankers Algorithm) Added in Advance features

--- advance_features/__init__.py
+++ advance_features/__init__.py
@@ -1,1 +1,11 @@
-#for importing the deadlock prevention to other modules
+"""
+Bonus: Deadlock Prevention Package
+---------------------------------
+Contains deadlock prevention algorithms like Banker's Algorithm.
+"""
+
+from .Deadlock_prevention import bankers_algorithm
+from .Bankers_data import total_resources, allocation, max_need, process_ids, resource_names
+
+
+__all__ = ["bankers_algorithm"]
