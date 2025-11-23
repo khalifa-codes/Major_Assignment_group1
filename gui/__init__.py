db3518f7466cb69132483c46573e8921ff2e86c0
M Hamza <ha243695@gmail.com>
2025-11-23 12:28:07 +0500

    Init file for gui added

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,1 +1,13 @@
-#using this file to make it package and call it in another module
+"""
+GUI Package for CPU Scheduling Simulator
+----------------------------------------
+This package contains the graphical user interface for the CPU Scheduling Simulator.
+
+Modules:
+    - main_window: Main menu window with algorithm selection
+    - scheduler_ui: UI for scheduling algorithms and deadlock prevention
+"""
+
+from .main_window import MainWindow
+
+__all__ = ["MainWindow"]
